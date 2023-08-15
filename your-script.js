002cfdcdf13fcc72b073bbf6a7a53199a3101c7f
Fede <fjimenez.x1@gmail.com>
2023-08-15 13:13:30 -0600

    camera and cursor added

--- your-script.js
+++ your-script.js
@@ -2,7 +2,6 @@ AFRAME.registerComponent('clickable', {
     init: function () {
         const button = this.el;
         
-        // Add event listener for click event
         button.addEventListener('click', () => {
             // Open the URL in a new tab
             window.open('https://www.google.com', '_blank');
