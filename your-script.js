41f616f0060ac2659c476592d8e7a2bc2d6307dc
Fede <fjimenez.x1@gmail.com>
2023-08-15 13:22:26 -0600

    button demo

--- your-script.js
+++ your-script.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('clickable', {
     init: function () {
         const button = this.el;
-        
+
         button.addEventListener('click', () => {
             // Open the URL in a new tab
             window.open('https://www.google.com', '_blank');
